e9fbf988f55dd2cc80de6a0126b58a1377950c7b
Jongyul Kim <yulistic@gmail.com>
2019-03-23 17:15:00 +0900

    Update script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git submodule update --init --recursive
-mv ~/.vim ~/.vim.old && echo "~/.vim exists. It was moved to '~/.vim.old'. Remove it if not required."
-mv ~/.vimrc ~/.vimrc.old && echo "~/.vimrc exists. It was moved to '~/.vimrc.old'. Remove it if not required."
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd) ~/.vim
+mv $HOME/.vim $HOME/.vim.old && echo "${HOME}/.vim exists. It was moved to '${HOME}/.vim.old'. Remove it if not required."
+mv $HOME/.vimrc $HOME/.vimrc.old && echo "${HOME}/.vimrc exists. It was moved to '${HOME}/.vimrc.old'. Remove it if not required."
+ln -s $(pwd)/vimrc $HOME/.vimrc
+ln -s $(pwd) $HOME/.vim
